169a6515b66b90425871ace1c6b6c8f67ef74db1
MT <murun1life@gmail.com>
2026-02-07 15:58:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRC_32_CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
