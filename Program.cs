9e6235f1bfab106c246ce16aa9f16120d4eabfc5
MT <murun1life@gmail.com>
2026-02-07 20:14:29 -0500

    add file stream

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CRC_32_CLI {
     internal class Program {
         static void Main(string[] args) {
-
 
             if (args.Length == 0 || (args.Length == 1 && args[0] == "--help")) {
 
@@ -16,7 +15,6 @@ namespace CRC_32_CLI {
                 parseInput(args);
             }
 
-            Console.ReadKey();
         }
 
         static void parseInput(string[] args) {
@@ -125,8 +123,9 @@ namespace CRC_32_CLI {
             } else {
                 // file mode
 
+                string data = inputs["input"];
                 uint crc;
-                /* TODO
+
                 if (inputs["direction"] == "reverse") {
                     // reverse polynomial, LSB -> MSB
                     // (used in modern x86, serial comms, Gzip, Python, Go)
@@ -140,42 +139,57 @@ namespace CRC_32_CLI {
                     Console.WriteLine("0x{0:X}", crc);
                 } else {
                     Console.WriteLine(crc);
-                }*/
+                }
             }
 
         }
 
-        /*
-        static uint CRC32File(string fileName, uint divisor, bool isReverse) {
-            int length = data.Length;
 
-            uint[] crcTable = new uint[256];
-
-            // precompute crc table
-            for (uint i = 0; i < 256; i++) {
-                crcTable[i] = CRCByte(i, divisor, isReverse);
-            }
-
-            uint crc = 0xFFFFFFFF;
-
+        static uint nextCRC(uint b, uint crc, uint divisor, bool isReverse, uint[] lookup) {
             if (isReverse) {
                 // fast crc based on shifting and lookup table
-                for (uint i = 0; i < data.Length; i++) {
-                    crc = crcTable[(crc ^ data[i]) & 0xFF] ^ (crc >> 8);
-                }
+                return lookup[(crc ^ b) & 0xFF] ^ (crc >> 8);
             } else {
-                for (int i = 0; i < data.Length; i++) {
-                    uint index = ((crc >> 24) ^ data[i]) & 0xFF;
-                    crc = (crc << 8) ^ crcTable[index];
+                uint index = ((crc >> 24) ^ b) & 0xFF;
+                return (crc << 8) ^ lookup[index];
+            }
+        }
+
+
+        static uint CRC32File(string fileName, uint divisor, bool isReverse) {
+            uint crc = 0xFFFFFFFF;
+
+            try {
+                // Use 'using' statement to ensure the FileStream is correctly disposed
+                using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read)) {
+                    int byteData;
+                    uint[] crcTable = new uint[256];
+
+                    // precompute crc table
+                    for (uint i = 0; i < 256; i++) {
+                        crcTable[i] = CRCByte(i, divisor, isReverse);
+                    }
+
+                    // read a byte
+                    // the returned value is not -1 (end of file)
+                    while ((byteData = fileStream.ReadByte()) != -1) {
+
+                        byte b = (byte)byteData;
+
+                        crc = nextCRC(b, crc, divisor, isReverse, crcTable);
+                    }
                 }
+            } catch (FileNotFoundException) {
+                Console.WriteLine($"Error: The file '{fileName}' was not found.");
+            } catch (Exception ex) {
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
 
             // final mask
             crc ^= 0xFFFFFFFF;
 
             return crc;
-
-        }*/
+        }
 
         static uint CRC32String(byte[] data, uint divisor, bool isReverse) {
             int length = data.Length;
@@ -189,23 +203,15 @@ namespace CRC_32_CLI {
 
             uint crc = 0xFFFFFFFF;
 
-            if (isReverse) {
-                // fast crc based on shifting and lookup table
-                for (uint i = 0; i < data.Length; i++) {
-                    crc = crcTable[(crc ^ data[i]) & 0xFF] ^ (crc >> 8);
-                }
-            } else {
-                for (int i = 0; i < data.Length; i++) {
-                    uint index = ((crc >> 24) ^ data[i]) & 0xFF;
-                    crc = (crc << 8) ^ crcTable[index];
-                }
+            // fast crc based on shifting and lookup table
+            for (uint i = 0; i < data.Length; i++) {
+                crc = nextCRC(data[i], crc, divisor, isReverse, crcTable);
             }
 
             // final mask
             crc ^= 0xFFFFFFFF;
 
             return crc;
-
         }
 
 
